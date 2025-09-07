1a476b32e7dd47c263e6309156691273fa366bcc
Anandhu Anand <anandhuzz916@gmail.com>
2025-09-07 13:07:10 +0530

    commit3-connect-backend-db

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,9 +1,14 @@
-#backend local server - dev phase
-
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+import os
 
 app = FastAPI()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
 @app.get("/health")
 def health():
-    return {"ok": True}
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT 1"))
+        return {"ok": True, "db": result.scalar() == 1}
